bea824fd44ee1b3a41c7474a4b76fdc31deb7988
FranFigueroa <70865088+FranFigueroa@users.noreply.github.com>
2025-01-10 04:14:27 -0300

    fix: files

--- backend/src/langchainTest.ts
+++ backend/src/langchainTest.ts
@@ -1,6 +1,11 @@
 import { ChatOpenAI } from "@langchain/openai";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const testLangChain = async () => {
+
+  console.log('OPENAI_API_KEY:', process.env.OPENAI_API_KEY); 
+
   const model = new ChatOpenAI({
     openAIApiKey: process.env.OPENAI_API_KEY,
   });
